e9c2436b8509cd6a272a4bae93d396d6875d6b8c
Yoav Morahg <yoav@techelevator.com>
2023-08-24 09:24:27 -0400

    first push

---
+++ src/main/java/com/techelevator/actions/ActionsApplication.java
@@ -0,0 +1,14 @@
+package com.techelevator.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsApplication.class, args);
+		System.out.println("Hello!");
+	}
+
+}
